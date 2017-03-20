ddd42a75f2b5e19ee8cc7bdb978027a85670041f
julietteabeyta <juneauwh0a@gmail.com>
2017-03-19 18:19:21 -0600

    Removed Portfolio Page, Added Home, Added Loading Welcome Text, Put Paragraphs in a .text-container

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,21 @@
+// Get window height
 var windowHeight = $(window).height();
 
+// Set container to window height
 $('.content-container').css({'height': windowHeight + "px"});
+
+// Divide "welcome" into array
+var welcome = "welcome!".split('');
+
+// Define function for loop through to print "w...e...l...c..."
+function type(i) {
+	setTimeout(function() {
+    	var letter = welcome[i].toString;
+        $("#loading").append(welcome[i]);
+    }, i * 100);
+}
+
+// Do typing loop
+for (i = 0; i <= welcome.length; i++) {
+    type(i);
+}
